59070ed96c57c4e4bc095c290e429b7bb4a3a27a
Miguellopescruzrodrigues <43889863+Miguellopescruzrodrigues@users.noreply.github.com>
2022-02-23 16:28:11 -0300

    Delete TagAndProbe-master directory

--- TagAndProbe-master/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/TagAndProbeSummerStudents2019/TagAndProbe/',1)[0])+'/cfipython/slc6_amd64_gcc472/TagAndProbeSummerStudents2019/TagAndProbe')
